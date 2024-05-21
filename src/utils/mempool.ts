0a08288e05a38a0a104a60a4077265726972808a
rizzolib <jo.hnceballos0716@gmail.com>
2024-05-21 16:13:59 +0900

    feat: standard

--- src/utils/mempool.ts
+++ src/utils/mempool.ts
@@ -13,19 +13,23 @@ export const getScriptPubkey = async (tx: string, address: string, networkType: 
   return output.scriptpubkey;
 };
 
-export const getUtxos = async (address: string, networkType: string): Promise<IUtxo[]> => {
-  const url = `https://mempool.space/${networkType == 'testnet' ? 'testnet/' : ''}api/address/${address}/utxo`;
-  const res = await axios.get(url);
-  const utxos: IUtxo[] = [];
+export const getUtxos = async (address: string, networkType: string): Promise<any> => {
+  try {
+    const url = `https://mempool.space/${networkType == 'testnet' ? 'testnet/' : ''}api/address/${address}/utxo`;
+    const res = await axios.get(url);
+    const utxos: IUtxo[] = [];
 
-  res.data.forEach((utxoData: any) => {
-    utxos.push({
-      txid: utxoData.txid,
-      vout: utxoData.vout,
-      value: utxoData.value,
+    res.data.forEach((utxoData: any) => {
+      utxos.push({
+        txid: utxoData.txid,
+        vout: utxoData.vout,
+        value: utxoData.value,
+      });
     });
-  });
-  return utxos;
+    return utxos;
+  } catch (err: any) {
+    console.log('Get Utxos Error')
+  }
 };
 
 export const pushBTCpmt = async (rawtx: any, networkType: string) => {
@@ -42,46 +46,60 @@ const postData = async (
   content_type = "text/plain",
   apikey = ""
 ): Promise<string | undefined> => {
-  while (1) {
-    try {
-      const headers: any = {};
-      if (content_type) headers["Content-Type"] = content_type;
-      if (apikey) headers["X-Api-Key"] = apikey;
-      const res = await axios.post(url, json, {
-        headers,
-      });
-      return res.data as string;
-    } catch (err: any) {
-      console.error(err);
-    }
+  try {
+    const headers: any = {};
+    if (content_type) headers["Content-Type"] = content_type;
+    if (apikey) headers["X-Api-Key"] = apikey;
+    const res = await axios.post(url, json, {
+      headers,
+    });
+    return res.data as string;
+  } catch (err: any) {
+    console.log('Push Transaction Error')
   }
 }
 
 export const getPrice = async (networkType: string) => {
-  const url = `https://mempool.space/${networkType == 'testnet' ? 'testnet/' : ''}api/v1/prices`;
-  const res: any = await axios.get(url);
-  return res.data;
+  try {
+    const url = `https://mempool.space/${networkType == 'testnet' ? 'testnet/' : ''}api/v1/prices`;
+    const res: any = await axios.get(url);
+    return res.data;
+  } catch (error: any) {
+    console.log("Get Price Error!")
+  }
 }
 
 export const getBlockHeight = async (networkType: string) => {
-  const url = `https://mempool.space/${networkType == 'testnet' ? 'testnet/' : ''}/api/blocks/tip/height`;
-  const res = await axios.get(url);
-  return res.data;
+  try {
+    const url = `https://mempool.space/${networkType == 'testnet' ? 'testnet/' : ''}/api/blocks/tip/height`;
+    const res = await axios.get(url);
+    return res.data;
+  } catch (error: any) {
+    console.log("Get Price Error!")
+  }
 }
 
 export const getFeeRate = async (networkType: string) => {
-  const height = await getBlockHeight(networkType);
-  const url = `https://mempool.space/${networkType == 'testnet' ? 'testnet/' : ''}api/v1/blocks/${height}`;
-  const blockData: any = await axios.get(url);
-  const feeRateData = blockData.data.map((item: any) => {
-    return { timestamp: item.timestamp, avgFeeRate: item.extras.avgFeeRate }
-  })
-  return feeRateData;
+  try {
+    const height = await getBlockHeight(networkType);
+    const url = `https://mempool.space/${networkType == 'testnet' ? 'testnet/' : ''}api/v1/blocks/${height}`;
+    const blockData: any = await axios.get(url);
+    const feeRateData = blockData.data.map((item: any) => {
+      return { timestamp: item.timestamp, avgFeeRate: item.extras.avgFeeRate }
+    })
+    return feeRateData;
+  } catch (error: any) {
+    console.log("Get Fee Rate Error!")
+  }
 }
 
 export const getRecommendedFeeRate = async (networkType: string) => {
-  const url = `https://mempool.space/${networkType == 'testnet' ? 'testnet4/' : ''}api/v1/fees/recommended`;
-  const response: any = await axios.get(url);
-  
-  return response.data;
+  try {
+    const url = `https://mempool.space/${networkType == 'testnet' ? 'testnet4/' : ''}api/v1/fees/recommended`;
+    const response: any = await axios.get(url);
+
+    return response.data;
+  } catch (error: any) {
+    console.log("Get Recommended Fee Rate Error!")
+  }
 }
