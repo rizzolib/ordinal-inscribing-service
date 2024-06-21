cc79ce65ea0552db0cda327ec806572185a2221a
rizzolib <jo.hnceballos0716@gmail.com>
2024-06-21 21:49:12 +0900

    feat: standard

--- src/services/wallet/initializeWallet.ts
+++ src/services/wallet/initializeWallet.ts
@@ -1,15 +1,11 @@
 import networkConfig from "../../config/network.config";
-import { TESTNET, WIF, SEED } from "../../config/network.config";
 import * as ecc from "tiny-secp256k1";
-import { initEccLib, networks } from "bitcoinjs-lib";
+import { initEccLib } from "bitcoinjs-lib";
 import { WIFWallet } from "./WIFWallet";
 initEccLib(ecc as any);
 
 const networkType: string = networkConfig.networkType;
 let wallet: any;
-
-const network =
-  networkConfig.networkType == TESTNET ? networks.testnet : networks.bitcoin;
 
 const privateKey: string = process.env.PRIVATE_KEY as string;
 wallet = new WIFWallet({ networkType: networkType, privateKey: privateKey });
